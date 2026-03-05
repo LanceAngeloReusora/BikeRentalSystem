c95e383a51b6d9e5248338bfddb0b519a7252f8e
jrnlrno <jehryn.laurino@cit.edu>
2026-03-05 20:14:31 +0800

    Bike.java

---
+++ src/Bike.java
@@ -0,0 +1,44 @@
+public abstract class Bike {
+
+    private String bikeId;
+    private String brand;
+    private double ratePerHour;
+    private boolean isAvailable;
+
+    public Bike(String bikeId, String brand, double ratePerHour) {
+        this.bikeId = bikeId;
+        this.brand = brand;
+        this.ratePerHour = ratePerHour;
+        this.isAvailable = true;
+    }
+
+    public String getBikeId() {
+        return bikeId;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public double getRatePerHour() {
+        return ratePerHour;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        this.isAvailable = available;
+    }
+
+    
+    public abstract double calculateRentalCost(int hours);
+
+    public void displayInfo() {
+        System.out.println("ID: " + bikeId +
+                " | Brand: " + brand +
+                " | Rate/hr: " + ratePerHour +
+                " | Available: " + isAvailable);
+    }
+}
