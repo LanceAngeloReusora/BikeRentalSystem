ab29b0593fa04c818ab4e89f3690e52db0ce9a08
jrnlrno <jehryn.laurino@cit.edu>
2026-03-05 20:24:36 +0800

    RoadBike.java

--- src/RoadBike.java
+++ src/RoadBike.java
@@ -1,3 +1,11 @@
-public class RoadBike {
+public class RoadBike extends Bike {
 
+    public RoadBike(String id, String brand) {
+        super(id, brand, 60);
+    }
+
+    @Override
+    public double calculateRentalCost(int hours) {
+        return getRatePerHour() * hours;
+    }
 }
