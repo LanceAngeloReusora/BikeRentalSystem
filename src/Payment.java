91c464aea2c6251a1120cb83be817584765cf64a
ErosJohnLao <erosjohn.lao@cit.edu>
2026-03-05 20:07:01 +0800

    Payment Class

--- src/Payment.java
+++ src/Payment.java
@@ -1,3 +1,19 @@
 public class Payment {
-    
+
+    private String paymentId;
+    private double amount;
+    private String method;
+
+    public Payment(String paymentId, double amount, String method) {
+        this.paymentId = paymentId;
+        this.amount = amount;
+        this.method = method;
+    }
+
+    public void processPayment() {
+        System.out.println("===== PAYMENT SUCCESSFUL =====");
+        System.out.println("Payment ID: " + paymentId);
+        System.out.println("Amount Paid: " + amount);
+        System.out.println("Method: " + method);
+    }
 }
