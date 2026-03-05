1e17e3280896292d288b72da30bac897ce3bd955
ivancarl <ivancarl.salvacion@cit.edu>
2026-03-05 22:45:14 +0800

    added another code

--- src/RoadBike.java
+++ src/RoadBike.java
@@ -1,11 +1,10 @@
 public class RoadBike extends Bike {
+     public RoadBike(String id, String brand) {
+        super(id, brand, 60); 
+    }
 
-    public RoadBike(String id, String brand) {
-        super(id, brand, 60);
-    }
-
-    @Override
-    public double calculateRentalCost(int hours) {
-        return getRatePerHour() * hours;
-    }
+    @Override
+    public double calculateRentalCost(int hours) {
+        return getRatePerHour() * hours;
+    }
 }
