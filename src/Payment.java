1960917b2e15db6b1902ff426e7267c9341367a5
ErosJohnLao <erosjohn.lao@cit.edu>
2026-05-16 14:13:10 +0800

    feat: add multiple payment methods

--- src/Payment.java
+++ src/Payment.java
@@ -1,10 +1,14 @@
 public class Payment {
 
+    public enum Method {
+        CASH, GCASH, MAYA, CREDIT_DEBIT, BANK_TRANSFER
+    }
+
     private String paymentId;
     private double amount;
-    private String method;
+    private Method method;
 
-    public Payment(String paymentId, double amount, String method) {
+    public Payment(String paymentId, double amount, Method method) {
         this.paymentId = paymentId;
         this.amount = amount;
         this.method = method;
@@ -12,8 +16,9 @@ public class Payment {
 
     public void processPayment() {
         System.out.println("===== PAYMENT SUCCESSFUL =====");
-        System.out.println("Payment ID: " + paymentId);
-        System.out.println("Amount Paid: " + amount);
-        System.out.println("Method: " + method);
+        System.out.println("Payment ID : " + paymentId);
+        System.out.println("Amount Paid: PHP " + amount);
+        System.out.println("Method     : " + method);
+        System.out.println("==============================\n");
     }
 }
