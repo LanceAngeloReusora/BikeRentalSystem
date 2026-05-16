7a23c0a3654b595c08e41311d6d85b83b5a5cbcd
ErosJohnLao <erosjohn.lao@cit.edu>
2026-05-16 13:48:44 +0800

    feat: add multiple payment methods

--- src/Receipt.java
+++
@@ -1,8 +0,0 @@
-public class Receipt {
-
-    public static void printReceipt(Rental rental) {
-        System.out.println("\n===== RECEIPT =====");
-        rental.displayRental();
-        System.out.println("====================\n");
-    }
-}
